7ddf59afc74aa117e4de5e3342b41b30fc76bbb4
Arkadiusz Sas <kontakt.arkadiusz.sas@gmail.com>
2018-10-28 21:50:02 +0100

    added MirrorImage class

---
+++ examples/transformations/MirrorImage.java
@@ -0,0 +1,42 @@
+package transformations;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+
+public class MirrorImage {
+
+	private static Imgcodecs imageCodecs;
+	private static Mat srcImage, dstImageHorizontally, dstImageVertically ;
+	private static Mat warpMatrix;
+
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	MirrorImage(String imagePath) {
+
+		imageCodecs = new Imgcodecs();
+
+		// load image to memory
+		srcImage = imageCodecs.imread(imagePath);
+		
+		// fill destination matrices with zeros
+		dstImageHorizontally = Mat.zeros(srcImage.rows(), srcImage.cols(), srcImage.type());
+		dstImageVertically = Mat.zeros(srcImage.rows(), srcImage.cols(), srcImage.type());
+		
+		// flip horizontal
+		Core.flip(srcImage, dstImageHorizontally, 1);
+		
+		// flip vertical
+		Core.flip(srcImage, dstImageVertically, 0);
+		
+		// display modified image
+		HighGui.imshow("Source Image", srcImage);
+		HighGui.imshow("Flip Horizontal", dstImageHorizontally);
+		HighGui.imshow("Flip Vertical", dstImageVertically);
+		HighGui.waitKey();
+	
+	}
+}
